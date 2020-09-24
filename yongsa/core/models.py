5f586fe3d06bc160a96b06e9322f1ad3c510463e
Jung <jungeun.lak@gmail.com>
2020-09-24 18:29:56 +0900

    Hero, Monster 모델 작성

--- yongsa/core/models.py
+++ yongsa/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length=20)
+    gold = models.IntegerField(default=1000)
+    atk = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Monster(models.Model):
+    name = models.CharField(max_length=50, default='몬스터')
+    atk = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} {self.atk}"
